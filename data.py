92bf50e33270b774e89d3b62ec5c4ea661b77564
NiveshSinghC <nivesh1998@gmail.com>
2017-06-01 19:50:59 +0530

    First commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+    {
+        'id':1,
+        'title':'one',
+        'body':'Loreasdfasdf asdfasdf',
+        'author':'nivesh singh',
+        'create_date':'4-23-2017'
+    },
+    {
+        'id':2,
+        'title':'two',
+        'body':'Loreasdfasdf dsfasdfasdf sdfasdf asdfasdf',
+        'author':'sivesh singh',
+        'create_date':'4-23-2017'
+    },
+    {
+        'id':3,
+        'title':'three',
+        'body':'Loreasdfasdfasd df asdfasdfasdg asd asdfdasdf asd sdf asdfa sdf asdfasdf',
+        'author':'nivesh singh',
+        'create_date':'4-23-2017'
+    }
+    ]
+    return articles
